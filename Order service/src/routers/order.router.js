ea365d0edd6d7321fce40840bff48f7865b11cb6
Gaurav Moghe <gauravmoghe0709@gmail.com>
2026-01-02 21:45:53 +0530

    create a some apis

--- Order service/src/routers/order.router.js
+++ Order service/src/routers/order.router.js
@@ -1,11 +1,19 @@
 const express = require ("express")
 const authmiddleware  = require ("../middleware/authmiddleware")
 const OrderController = require("../controller/order.controller")
+const validation = require("../middlewares/validation.middleware")
 
 const router = express.Router()
 
 router.post("/createorder",authmiddleware(["user"]),OrderController.createOrder)
 
+router.get("/me", authmiddleware([ "user" ]), OrderController.getMyOrders)
+
+router.post("/:id/cancel", authmiddleware([ "user" ]), OrderController.cancelOrderById)
+
+router.patch("/:id/address", authmiddleware([ "user" ]), validation.updateAddressValidation, OrderController.updateOrderAddress)
+
+router.get("/:id", authmiddleware([ "user", "admin" ]), OrderController.getOrderById)
 
 
 
